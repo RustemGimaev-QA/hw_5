0bcfe954dc6ebebf76b3e3fb6e1c66b2c2cd5981
Рустем Гимаев <rustem.gimaev@bristolcapital.ru>
2025-02-23 21:23:31 +0500

    test

--- test/test_practice_form.py
+++ test/test_practice_form.py
@@ -24,7 +24,23 @@ def test_practice_form():
     browser.element('#city').click().element('#react-select-4-option-2').click()
     browser.element('#submit').click()
     browser.element('.modal-content').should(have.text('Thanks for submitting the form'))
+
+    #Проверка введенных данных в результирующей таблице
+    browser.element(".table-dark").should(have.text('Petrov Andrey'))
+    browser.element(".table-dark").should(have.text('petrov@mail.ru'))
+    browser.element(".table-dark").should(have.text('Male'))
+    browser.element(".table-dark").should(have.text('8925321852'))
+    browser.element(".table-dark").should(have.text('10 March,2008'))
+    browser.element(".table-dark").should(have.text('Maths'))
+    browser.element(".table-dark").should(have.text('Sports'))
+    browser.element(".table-dark").should(have.text('Screenshot_4.png'))
+    browser.element(".table-dark").should(have.text('City Kirov Lenin street house 25 apartment 3'))
+    browser.element(".table-dark").should(have.text('Uttar Pradesh Merrut'))
+
+    #Закрытие результирующей таблицы
     browser.element('.modal-content').element('#closeLargeModal').click()
+
+    #Проверка элемента на странице
     browser.element('[class=text-center]').should(have.text('Practice Form'))
 
 

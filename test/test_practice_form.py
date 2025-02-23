5df959d34acb62bae1433e956723e3fb1244199f
Рустем Гимаев <rustem.gimaev@bristolcapital.ru>
2025-02-23 18:54:31 +0500

    test

--- test/test_practice_form.py
+++ test/test_practice_form.py
@@ -7,5 +7,11 @@ def test_practice_form():
     browser.element('#lastName').type('Андрей')
     browser.element('#userEmail').type('petrov@mail.ru')
     browser.element('[for=gender-radio-1]').click()
-    browser.element('#userNumber').type('89253218526')
-
+    browser.element('#userNumber').type('8925321852')
+    browser.element('.react-datepicker-wrapper').click()
+    browser.element('.react-datepicker__day-names').should(have.text('We'))
+    browser.element('.react-datepicker__month-select').click().element("[value='2']").click()
+    browser.element('.react-datepicker__current-month').should(have.text('March'))
+    browser.element('.react-datepicker__year-select').click().element("[value='2008']").click()
+    browser.element('.react-datepicker__current-month').should(have.text('2008'))
+    browser.element('.react-datepicker__day--010').click()
